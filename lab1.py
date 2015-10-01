d22430ad7034140f04b96e7ac427907718b911c5
Yashvi <yashvi.c.shah@gmail.com>
2015-10-01 12:15:03 -0400

    final lab_YashviShah

--- lab1.py
+++ lab1.py
@@ -2,19 +2,28 @@
 
 """ Graded Lab #1 for Inf1340, Fall 2015 """
 
-__author__ = 'Susan Sim'
+__author__ = 'Yashvi Shah'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
 def vowel_or_consonant():
-    """
-    Exercise: Vowel or Consonant
-    Reads a letter of the alphabet from the user. (You can assume that it's
-    lowercase.) If the user enters a, e, i, o or u then your program should
-    display "vowel". If the user enters y then your program should display
-    "sometimes a vowel, sometimes a consonant". Otherwise your program should
-    display a message indicating that the letter is a "consonant".
-    """
+    letter = raw_input("Please enter a letter.")
+    if letter == "a":
+        print("vowel")
+    elif letter == "e":
+        print ("vowel")
+    elif letter == "i":
+        print ("vowel")
+    elif letter == "o":
+        print("vowel")
+    elif letter == "u":
+        print("vowel")
+    elif letter == "y":
+        print("sometimes a vowel, sometimes a consonant")
+    else:
+        print("consonant")
+#vowel_or_consonant()
+
 
